7399ff2fa6ca9bb263a87443200023e35a9f989e
Amandine <amandegh@hotmail.com>
2020-11-19 15:16:32 -0500

    Modification du script 01

--- 01-hello_world.sh
+++ 01-hello_world.sh
@@ -1,2 +1,17 @@
 #!/bin/bash
 
+#Ceci est un commentaire
+
+echo "salut"
+
+#ou 
+
+echo salut
+
+#ou 
+
+echo 'salut Antoine'
+
+echo -n "Vive"
+echo "Linux"
+#Va écrire Vive Linux sans saut de ligne
