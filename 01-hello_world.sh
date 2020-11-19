69c0db53a52e19d3bfa238f74982561fa0284814
Amandine <amandegh@hotmail.com>
2020-11-19 15:32:30 -0500

    Modification d'un premier script

--- 01-hello_world.sh
+++ 01-hello_world.sh
@@ -15,3 +15,10 @@ echo 'salut Antoine'
 echo -n "Vive"
 echo "Linux"
 #Va écrire Vive Linux sans saut de ligne
+ echo -e "1\n2\n3\n4" #-e interprète les métacaractères
+
+read -p "My name is ?" nom 
+#read =cin, permet que l'utilisateur fasse une entree
+# -p "Mon message" >> variable_qui_recoit_la_variable, ici nom
+
+echo "tchick ka tchick la tchic" $nom
